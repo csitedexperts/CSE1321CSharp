bb803acbd0f5218313fdec441519bb57dfb3fc3f
CSITEd Experts <csitedexperts@gmail.com>
2019-10-19 23:12:48 -0400

    Solution renamed1

---
+++ CSE1321CSharpProjects/PencilClassEx/Pencil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PencilClassEx
+{
+    class Pencil
+    {
+        public int serialNo;
+        public String model;
+        public String type;
+        public bool hasEraser;
+
+        public Pencil()
+        {
+            serialNo = 101;
+            model = "Amazon";
+            type = "HB";
+            hasEraser = true;
+            // System.out.println("The Pencil() constructor is called.....");
+        }
+
+        public Pencil(int SerialNo, String model, String type, bool hasEraser)
+        {
+            this.serialNo = SerialNo;
+            this.model = model;
+            this.type = type;
+            this.hasEraser = hasEraser;
+            // System.out.println("The Pencil() constructor is called.....");
+        }
+
+        public void canWrite()
+        {
+            Console.WriteLine("This pencil can write");
+        }
+        //Returns a string representation of the object. 
+        //In general, the toString method returns a string
+        //that "textually represents" this object.
+
+        // In Java @Override
+        public override String ToString()
+        {
+
+            return serialNo + " " + model + " " + type + " " + hasEraser;
+        }
+
+    }
+}
---
+++ CSE1321CSharpProjects/PencilClassEx/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PencilClassEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pencil p1 = new Pencil();
+            Pencil p2 = new Pencil();
+            // Pencil(int serialNo, String model, String type, boolean hasEraser)
+            Pencil p3 = new Pencil(103, "Walmart", "B+", false);
+            Pencil p4 = new Pencil(104, "Walmart", "BH+", true);
+            Pencil p5 = new Pencil(105, "Costco", "B++", true);
+
+            Console.WriteLine(p1);
+
+
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+            Console.WriteLine(p5);
+            /*
+            p1.hasEraser = false;
+            p1.model = "Dixon";
+            p1.serialNo = 1;
+            p1.type = "HB";
+            p1.canWrite();
+
+            int x = 10;
+            Console.WriteLine(x);
+
+
+            Pencil p2 = new Pencil();
+
+            p2.hasEraser = true;
+            p2.model = "Dixon";
+            p2.serialNo = 2;
+            p2.type = "HB2";
+            p2.canWrite();
+
+            Pencil p3 = new Pencil();
+            p3.hasEraser = true;
+            p3.model = "BIC";
+            p3.serialNo = 3;
+            p3.type = "HB";
+            p3.canWrite();
+
+            Pencil p4 = new Pencil();
+            p4.hasEraser = true;
+            p4.model = "Write";
+            p4.serialNo = 4;
+            p4.type = "HB";
+            p4.canWrite();
+
+            Pencil p5 = new Pencil();
+
+            p5.hasEraser = true;
+            p5.model = "Zebra";
+            p5.serialNo = 0;
+            p5.type = "M301";
+            p5.canWrite();
+            Console.WriteLine("=============");
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+            Console.WriteLine(p5);
+    */
+
+        }
+    }
+}
